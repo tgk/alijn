75c5550f3795471a00d014988e411757ff0924c1
Thomas Greve Kristensen <tgk@cs.au.dk>
2010-07-16 14:48:12 +0200

    Support for multiple SMARTS definitions for same feature added

--- src/alijn/features.clj
+++ src/alijn/features.clj
@@ -1,6 +1,5 @@
 (ns alijn.features
-  (:use [clj-todo.todo]
-	[alijn combinatorics math]
+  (:use [alijn combinatorics math utils io]
 	[clojure.contrib.str-utils2 :only [split-lines split]])
   (:import 
    [org.openscience.cdk.smiles.smarts SMARTSQueryTool]))
@@ -16,15 +15,15 @@
        (filter not-commented?)
        (map #(.split #" " %))
        (map seq)
-       (apply concat)
-       (apply hash-map)))
+       (group-by first)
+       (map-on-values (partial map second))))
 
 (defn get-query-tool [smarts-string] (SMARTSQueryTool. smarts-string))
 (def cached-query-tool (memoize get-query-tool))
-(defn find-feature
+(defn find-feature-atoms
   "Result is a coll of atom collections, even though some features
 only match a single atom."
-  [smarts-string molecule]
+  [molecule smarts-string]
   (let [query-tool (cached-query-tool smarts-string)
 	status (.matches query-tool molecule)]
     (if status
@@ -33,20 +32,32 @@ only match a single atom."
 	   (.getUniqueMatchingAtoms query-tool))
       [])))
 
-(defn get-center [& atoms]
+(defn get-center [atoms]
   "Returns the center of the atoms as a Point3d."
   (let [points (map #(.getPoint3d %) atoms)]
     (vec-center points)))
 
+(defn find-features
+  "Finds all the features with the given SMARTS strings.
+Returns the center points."
+  [molecule smarts-strings]
+  (->>  smarts-strings
+	(map (partial find-feature-atoms molecule))
+	(apply concat)
+	(map get-center)
+	distinct))
+   
 (defn feature-groups
-  "Extract the features defined in a {name smarts-string} map
-from the molecule. Returns collection of {:name :centers}.
+  "Extract the features defined in a {name smarts-strings} map
+from the molecule. Returns collection of name -> centers.
 The centers are Point3d objects."
   [features molecule]
-  (apply
-   merge
-   (map (fn [[name smarts-string]]
-	  (let [groups (find-feature smarts-string molecule)
-		centers (map (partial apply get-center) groups)]
-	    {name centers}))
-	features)))
+  (map-on-values (partial find-features molecule) features))
+
+(comment let [features (parse-features "data/example/phase.smarts")
+      molecules (read-molecules "data/example/comt_subset.sdf")]
+  (println)
+  (println features)
+  (doseq [molecule molecules]
+    (println
+     (feature-groups features molecule))))
