2bb64edae576055b8a4b8244b9a59a14f794a2c7
Thomas Greve Kristensen <tgk@cs.au.dk>
2010-07-19 12:16:07 +0200

    Changed project.clj to use 1.2, still problems

--- project.clj
+++ project.clj
@@ -1,16 +1,20 @@
 (defproject alijn "0.0.1-SNAPSHOT"
   :description "A program for extracting and aligning molecular pharmacophores."
   :repositories {"org.openscience" "https://maven.ch.cam.ac.uk/m2repo"}
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
-		 [org.openscience/cdk "1.3.4"]
-		 [jama "1.0.1"]
-		 [penumbra "0.5.0"]]
-  :native-dependencies [[lwjgl "2.2.2"]]
-  :dev-dependencies [[lein-run "1.0.0-SNAPSHOT"]
-		     [swank-clojure "1.2.0"]
-		     [clj-todo "0.1.0-SNAPSHOT"]
-		     [native-deps "1.0.0"]]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+;		 [org.openscience/cdk "1.3.4"]
+;		 [jama "1.0.1"]
+		 [penumbra "0.6.0-SNAPSHOT"]]
+  :native-dependencies [[penumbra/lwjgl "2.4.2"]]
+  :dev-dependencies [
+; Depends on clojure 1.1
+;		     [lein-run "1.0.0-SNAPSHOT"]
+;		     [clj-todo "0.1.0-SNAPSHOT"]
+; Just trying to eliminate stuff
+;		     [swank-clojure "1.2.1"]
+		     [native-deps "1.0.0"]
+		     ]
   :namespaces [alijn.molecule-alignment]
   :main alijn.molecule-alignment
   :jvm-opts "-Xmx1g"
