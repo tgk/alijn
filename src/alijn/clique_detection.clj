33ac512e0db56b2f81e4e86789e3577504328165
Thomas Greve Kristensen <tgk@cs.au.dk>
2010-08-19 11:09:25 +0200

    Added clique detection initial file along with unit tests for node-distances

---
+++ src/alijn/clique_detection.clj
@@ -0,0 +1,4 @@
+(ns alijn.clique-detection)
+
+(defn node-distances [root edges]
+  {})
---
+++ test/alijn/clique_detection_test.clj
@@ -0,0 +1,26 @@
+(ns alijn.clique-detection-test
+  (:use [alijn.clique-detection] :reload-all)
+  (:use [clojure.test]
+	clj-todo))
+
+(deftest test-node-distance
+
+  (is (= {:root 0} (node-distances :root {:root []})))
+
+  (let [graph {:a [:b], :b [:a :c], :c [:b]}]
+    (is (= {:a 0, :b 1, :c 2} (node-distances :a graph)))
+    (is (= {:a 1, :b 0, :c 1} (node-distances :b graph)))
+    (is (= {:a 2, :b 1, :c 0} (node-distances :c graph))))
+
+  (let [graph {:a [:b :c :d], :b [:a], :c [:a], :d [:a]}]
+    (is (= {:a 0, :b 1, :c 1, :d 1} (node-distances :a graph)))
+    (is (= {:a 1, :b 0, :c 2, :d 2} (node-distances :b graph)))
+    (is (= {:a 1, :b 2, :c 0, :d 2} (node-distances :c graph)))
+    (is (= {:a 1, :b 2, :c 2, :d 0} (node-distances :d graph))))
+
+  (let [graph {:a [:b], :b [:a :c], :c [:b :d :e], :d [:c], :e [:c]}]
+    (is (= {:a 0, :b 1, :c 2, :d 3, :e 3} (node-distances :a graph)))
+    (is (= {:a 1, :b 0, :c 1, :d 2, :e 2} (node-distances :b graph)))
+    (is (= {:a 2, :b 1, :c 0, :d 1, :e 1} (node-distances :c graph)))
+    (is (= {:a 3, :b 2, :c 1, :d 0, :e 2} (node-distances :d graph)))
+    (is (= {:a 3, :b 2, :c 1, :d 2, :e 0} (node-distances :e graph)))))
