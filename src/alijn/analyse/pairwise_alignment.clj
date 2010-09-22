c573c7f65d2ec01c01be63b7d015f78393ef663b
Thomas Greve Kristensen <tgk@cs.au.dk>
2010-09-22 16:03:47 +0200

    Added support for evaluating alignment of sampled conformations.

--- src/alijn/analyse/pairwise_alignment.clj
+++ src/alijn/analyse/pairwise_alignment.clj
@@ -1,17 +1,38 @@
 (ns alijn.analyse.pairwise-alignment
   (:use [alijn io features pairwise-alignment math utils]
-	clojure.contrib.command-line))
-
-(defn get-rmsd-of-optimal-alignment
-  [threshold charge-limit constant-molecule variable-molecule]
-  (molecule-rmsd 
-   variable-molecule
-   (align threshold constant-molecule variable-molecule charge-limit)))
+	clojure.contrib.command-line
+	clojure.contrib.generic.functor
+	clojure.pprint))
 
 (def desc 
 "Reads the molecules from one or more files. For each file, performs
 pairwise alignment between every molecule in these and prints a 
-table with the results.")
+table with the results.
+Molecule names are truncated such that 1kcb_h_min becomes 1kcb.
+If a file contains multiple molecules with the same name, the first
+molecule is the native binding mode and the remaining are 
+conformations. If only one molecule exists for each name they are
+all native conformations that are sought re-aligned.")
+
+(defn molecule-name [molecule] (.get (.getProperties molecule) "cdk:Title"))
+(defn short-name    [molecule] (first (.split (molecule-name molecule) "_")))  
+
+(defn get-best-rmsd 
+  [threshold charge-limit constant-molecule [native-variable & variable-molecules]]
+  (molecule-rmsd
+   native-variable
+   (align-with-multiple-variable 
+     threshold constant-molecule variable-molecules charge-limit)))
+
+(defn count-successes
+  [threshold charge-limit success-rmsd grouped-ligands]
+  (fmap
+   (fn [conformations]
+     (let [reference (first conformations)
+	   best-rmsds (map (partial get-best-rmsd threshold charge-limit reference)
+			   (vals grouped-ligands))]
+       (count (filter #(<= % success-rmsd) best-rmsds))))
+   grouped-ligands))
 
 (defn 
   #^{:doc desc}
@@ -27,28 +48,29 @@ table with the results.")
 	  success-rmsd (Double/parseDouble success-rmsd)]
       (print-table
        (cons
-	["target" "avg. success" "min" "max" "# ligands"]
-	(for [filename filenames
-	      :let [molecules (read-molecules filename)
-		    name (first (.split (last (.split filename "/")) "\\."))
-		    rmsds (for [molecule molecules]
-			    (map (partial 
-				  get-rmsd-of-optimal-alignment 
-				  threshold charge-limit molecule) 
-				 molecules))
-		    successes (for [l rmsds] (map #(<= % success-rmsd) l))
-		    rates (for [l successes] (/ (count (filter true? l)) (count l)))
-		    avg (str (int (* 100 (average rates))))
-		    min_succ (str (int (* 100 (apply min rates))))
-		    max_succ (str (int (* 100 (apply max rates))))
-		    ]]
-	  [name avg min_succ max_succ (str (count molecules))]))))))
-  
+	["target" "avg. success" "min" "max" "# ligands"]	
+	(for [filename filenames]
+	  (let [molecules (read-molecules filename)
+		target-name (first (.split (last (.split filename "/")) "\\."))
+		grouped-ligands (group-by short-name molecules)
+		grouped-ligands (if (apply = 1 (vals (fmap count grouped-ligands))) 
+				  (group-by short-name (concat molecules molecules)) 
+				  grouped-ligands)
+		successes (count-successes threshold charge-limit success-rmsd 
+					   grouped-ligands)
+		success-rates (fmap #(int (* 100 (/ % (count grouped-ligands)))) 
+				    successes)]
+	    [target-name 
+	     (str (average (vals success-rates)))
+	     (str (apply min (vals success-rates)))
+	     (str (apply max (vals success-rates)))
+	     (str (count grouped-ligands))])))))))
+       
 (def test-file-1 "data/example/carboxypth-a.mol2")
 (def test-file-2 "data/example/concanavalin.mol2")
 (def test-file-3 "data/grouped/flexs/g-phosphorylase.mol2")
 
-(defn test-align-and-show []
+(defn test-and-show []
   (align-and-show-table
    "--threshold" "1" 
-   test-file-3))
+   test-file-1))
--- src/alijn/pairwise_alignment.clj
+++ src/alijn/pairwise_alignment.clj
@@ -8,25 +8,51 @@
 	clojure.pprint
 	clojure.contrib.generic.functor))
 
+(defn extract-features-and-align
+  "Extracts features and aligns these. Returns
+  alignment result as by colored-point-alignment."
+  [threshold constant-molecule variable-molecule charge-limit]
+  (let [constant-features (find-features constant-molecule charge-limit)
+	variable-features (find-features variable-molecule charge-limit)
+	constant-features-as-points (fmap (partial map get-point) constant-features)
+	variable-features-as-points (fmap (partial map get-point) variable-features)
+	feature-ks (keys constant-features-as-points)
+	constant-points (map constant-features-as-points feature-ks)
+	variable-points (map variable-features-as-points feature-ks)]
+    (colored-point-alignment threshold constant-points variable-points)))
+  
+(defn shift-and-rotate-to-frame 
+  "Places a variable molecule in the same reference frame as 
+  its features have been in an alignment result."
+  [alignment-result variable-molecule]
+  (translate-rotate-and-translate-molecule 
+   (neg (:variable-center alignment-result))
+   (:rotation alignment-result)
+   (:constant-center alignment-result)
+   variable-molecule))
+
 (defn align 
   "Aligns a pair of molecules using their features.
   Returns variable-molecule shifted and rotated to
   match features of constant-molecule."
   [threshold constant-molecule variable-molecule charge-limit]
-  (let [constant-features (find-features constant-molecule charge-limit)
-	variable-features (find-features variable-molecule charge-limit)
-	constant-features-as-points (fmap (partial map get-point) 
-						   constant-features)
-	variable-features-as-points (fmap (partial map get-point)
-						   variable-features)
-	feature-ks (keys constant-features-as-points)
-	constant-points (map constant-features-as-points feature-ks)
-	variable-points (map variable-features-as-points feature-ks)
-	alignment-result (colored-point-alignment 
+  (let [alignment-result (extract-features-and-align 
 			  threshold 
-			  constant-points variable-points)]
-    (translate-rotate-and-translate-molecule 
-     (neg (:variable-center alignment-result))
-     (:rotation alignment-result)
-     (:constant-center alignment-result)
-     variable-molecule)))
+			  constant-molecule variable-molecule 
+			  charge-limit)]
+    (shift-and-rotate-to-frame alignment-result variable-molecule)))
+
+(defn align-with-multiple-variable
+  "Aligns a single molecule with all the conformations
+  of the variable molecule using features. Returns the
+  shifted and rotated smallest molecule that gave rise
+  to the lowest rmsd."
+  [threshold constant-molecule variable-molecules charge-limit]
+  (let [alignment-results (map #(extract-features-and-align 
+				 threshold constant-molecule % charge-limit) 
+			       variable-molecules)
+	alignment-results (map #(assoc %1 :variable-molecule %2) 
+			       alignment-results variable-molecules)
+	smallest-rmsd-result (apply min-key :rmsd alignment-results)]
+    (shift-and-rotate-to-frame smallest-rmsd-result 
+			       (:variable-molecule smallest-rmsd-result))))
