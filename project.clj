72de96808d38fb919b689561f1aff63bcc1a6986
Thomas Greve Kristensen <tgk@cs.au.dk>
2010-10-08 14:08:19 +0200

    Updated project.clj to use own repository for exotic libs.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject alijn "0.0.1-SNAPSHOT"
   :description "A program for extracting and aligning molecular pharmacophores."
+   :repositories {"cs.au.dk" "http://cs.au.dk/~tgk/repo"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [org.openscience/cdk "1.3.5.git"]
